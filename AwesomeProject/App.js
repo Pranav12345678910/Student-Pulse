c2b59f38c8c15fe83ee4046ec509d9662c97bad5
Austin F <austinfujimori123@gmail.com>
2022-05-26 20:03:08 -0700

    nesting tab navigator within stack navigator, info button

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,29 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Button, TextInput, Text, View, TouchableOpacity, ScrollView} from 'react-native';
-import { Card } from 'react-native-elements';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from "expo-status-bar";
+import {
+  Image,
+  StyleSheet,
+  Button,
+  TextInput,
+  Text,
+  View,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import { Card } from "react-native-elements";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 
+//Tab Navigation
+const Tab = createMaterialTopTabNavigator();
 
 function SurveyScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <ScrollView style={styles.scrollView}>
         <Card>
-          <Card.Title style={{color: '#73b4a8'}}>
+          <Card.Title style={{ color: "#73b4a8" }}>
             How much sleep did you get today?
           </Card.Title>
-            <Card.Divider/>
-              <View>
-                <TextInput
-                  style={{
-                    height: 40,
-                    margin: 12,
-                    borderWidth: 1,
-                    padding: 10,
-                  }}
-                />
-              </View>
+          <Card.Divider />
+          <View>
+            <TextInput
+              style={{
+                height: 40,
+                margin: 12,
+                borderWidth: 1,
+                padding: 10,
+              }}
+            />
+          </View>
         </Card>
       </ScrollView>
     </View>
@@ -32,29 +44,69 @@ function SurveyScreen({ navigation }) {
 
 function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'top'}}>
-      <TouchableOpacity
-        onPress={() => navigation.navigate('Survey')}
-        style={{
-          width:'100%',
-          alignItems: 'center', 
-          justifyContent: 'top',
-          backgroundColor: '#eb7b76',
-          paddingTop:'1%',
-          paddingBottom: '1%',
+    <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          tabBarActiveTintColor: "#e91e63",
+          tabBarLabelStyle: { fontSize: 12 },
+          tabBarStyle: { backgroundColor: "powderblue" },
         }}
+      //   <TouchableOpacity
+      //   onPress={() => navigation.navigate("Survey")}
+      //   style={{
+      //     width: "100%",
+      //     alignItems: "center",
+      //     justifyContent: "top",
+      //     backgroundColor: "#eb7b76",
+      //     paddingTop: "1%",
+      //     paddingBottom: "1%",
+      //   }}
+      // >
+      //   <Text style={{ color: "white" }}>Take today's survey</Text>
+      // </TouchableOpacity>
       >
-        <Text style={{color:'white'}}>
-          Take today's survey
-        </Text>
-      </TouchableOpacity>
+        <Tab.Screen
+          name="My Data"
+          component={MyDataScreen}
+          options={{ tabBarLabel: "My Data" }}
+        />
+        <Tab.Screen
+          name="My Classes"
+          component={MyClassesScreen}
+          options={{ tabBarLabel: "My Classes" }}
+        />
+      </Tab.Navigator>
+  );
+}
+
+function MyDataScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "top" }}>
       <ScrollView style={styles.scrollView}>
         <Card>
           <Card.Title>Your pulse is ...</Card.Title>
-            {/* <Card.Divider/> */}
-              <View>
-              <Image source = {require('/Users/austinfujimori/Desktop/Student-Pulse/AwesomeProject/images/pulse.png')} />
-              </View>
+          <View>
+            <Image
+              source={require("/Users/austinfujimori/Desktop/Student-Pulse/AwesomeProject/images/pulse.png")}
+            />
+          </View>
+        </Card>
+      </ScrollView>
+    </View>
+  );
+}
+function MyClassesScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "top" }}>
+      
+      <ScrollView style={styles.scrollView}>
+        <Card>
+          <Card.Title>Your pulse is ...</Card.Title>
+          <View>
+            <Text>
+              Classes
+            </Text>
+          </View>
         </Card>
       </ScrollView>
     </View>
@@ -62,7 +114,6 @@ function HomeScreen({ navigation }) {
 }
 
 const Stack = createNativeStackNavigator();
-
 
 export default function App() {
   return (
@@ -72,15 +123,23 @@ export default function App() {
           name="Home"
           component={HomeScreen}
           options={{
-            title: 'Student Pulse',
+            title: "Student Pulse",
+
             headerStyle: {
-              backgroundColor: '#73b4a8',
+              backgroundColor: "#73b4a8",
             },
-            headerTintColor: '#fff',
+            headerTintColor: "#fff",
             headerTitleStyle: {
-              fontWeight: 'light',
-              fontSize: '30px',
+              fontWeight: "light",
+              fontSize: "30px",
             },
+            headerRight: () => (
+              <Button
+                onPress={() => alert("Information")}
+                title="Info"
+                color="#fff"
+              />
+            ),
           }}
         />
         <Stack.Screen name="Survey" component={SurveyScreen} />
@@ -89,14 +148,11 @@ export default function App() {
   );
 }
 
-
-
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
